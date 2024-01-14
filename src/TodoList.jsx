9c1c103b5650516dd9e73e8fd1292603a9dbdd1f
sara latorre <saralatorre97@gmail.com>
2024-01-14 03:00:55 +0100

    finished react tutorial project

---
+++ src/TodoList.jsx
@@ -0,0 +1,20 @@
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({ todos, toggleTodo, deleteToDo }) {
+    return (
+        <ul className="list">
+            {todos.length === 0 && "No todos"}
+            {todos.map(todo => {
+            // console.log(todos);
+            return (
+            <TodoItem 
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteToDo}
+            />
+            )
+            })}
+        </ul>
+    )
+}
