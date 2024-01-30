994bd57dca23717ea5ef224a7c9618e217b14c79
sara latorre <saralatorre97@gmail.com>
2024-01-30 03:21:04 +0100

    added head and script component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,26 @@
 import Link from "next/link"
+import Head from "next/head"
+
+// next/script is an extension of the HTML <script> element and optimizes when additional scripts are fetched and executed.
+import Script from "next/script"
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    // strategy controls when the third-party script should load. A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time
+
+                    onLoad={() =>
+                        // onLoad is used to run any JavaScript code immediately after the script has finished loading. In this example, we log a message to the console that mentions that the script has loaded correctly
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                >
+                </Script>
+            </Head>
             <h1>Fist Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
